f4a376aa3bbeca32d45651adc3a83d1894a3d54a
Sarah <smlawt03@gmail.com>
2025-03-26 18:52:41 -0500

    small change to test ci/cd

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/api/message', methods=['GET'])
 def get_message():
-    return jsonify({'message': 'Hello from Flask!'}), 200
+    return jsonify({'message': 'Hello from Sarahs Flask!'}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
