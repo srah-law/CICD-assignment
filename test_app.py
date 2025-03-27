eb0d03ff5fc402a1e606e4b7cab69d7e1e94f92e
srah-law <smlawt03@gmail.com>
2025-03-26 19:14:22 -0500

    another small change

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_get_message():
     with app.test_client() as client:
         response = client.get('/api/message')
         assert response.status_code == 200
-        assert response.json == {'message': 'Hello from Flask!'}
+        assert response.json == {'message': 'Hello from SARAHS Flask!'}
